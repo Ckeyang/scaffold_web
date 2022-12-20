db1cb17ddd680fb2914119c9217b440585f08cf3
yangwenjie <yangwenjie@zhifei-group.com>
2022-12-20 17:10:55 +0800

    清空 重新造

--- src/config/env.js
+++
@@ -1,5 +0,0 @@
-const baseUrl = import.meta.env.VITE_BASE_URL
-const codeUrl = `${window.location.origin}/code`
-const actUrl = `${window.location.origin}/act/modeler.html?modelId=`
-
-export { baseUrl, codeUrl, actUrl }
