b48a316791cd672fcf7e9a555b2d58232c1f4126
yangwenjie <yangwenjie@zhifei-group.com>
2022-12-27 14:50:20 +0800

    fix:优化代码

--- src/plugins/axios/index.ts
+++ src/plugins/axios/index.ts
@@ -1,13 +1,29 @@
 import axios from 'axios';
 import config from "@/plugins/axios/config";
+import {ElNotification} from "element-plus";
+import {useUserState} from "@/stores/user";
+
+const userState = useUserState();
+
 const http = axios.create(config);
 // 请求拦截器 ，主要是用来做header token设置
-http.interceptors.request.use((axiosConfig: axios.AxiosRequestConfig<any>) =>{
+// @ts-ignore
+http.interceptors.request.use((axiosConfig: axios.AxiosRequestConfig<any>) => {
     console.log(axiosConfig)
+    if (userState.token) {
+        axiosConfig.headers['token'] = userState.token;
+    }
     return axiosConfig
 });
-http.interceptors.response.use( (response: axios.AxiosResponse<any, any>) =>{
+// @ts-ignore
+http.interceptors.response.use((response: axios.AxiosResponse<any, any>) => {
     console.log(response)
-        return response
+    if (response.data.code !== 200) {
+        ElNotification({
+            title: '错误信息',
+            message: response.data.message
+        })
+    }
+    return response
 });
 export default http;
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="w-full h-screen flex justify-center items-center bg-amber-200">
     <section class="border rounded w-1/3  min-w-[400px] px-4 py-8 bg-white">
-      <el-input placeholder="请输入账号" v-model="form.userAccount"></el-input>
-      <el-input placeholder="请输入密码" v-model="form.password" type="password" class="mt-8"></el-input>
-      <el-input placeholder="请输入验证码" v-model="form.captchCode" class="mt-8">
+      <el-input placeholder="请输入账号" size="large" v-model="form.userAccount"></el-input>
+      <el-input placeholder="请输入密码" size="large" v-model="form.password" type="password" class="mt-8"></el-input>
+      <el-input placeholder="请输入验证码" size="large" v-model="form.captchCode" class="mt-8">
         <template #append class="relative">
           验证码
           <img class="w-full absolute left-0 cursor-pointer" :src="form.reCode" @click="getCodeURL"/>
@@ -18,7 +18,11 @@
 <script lang="ts" setup>
 import {onMounted, ref} from "vue";
 import {getCode, login} from '@/plugins/api/api-login-controller.js'
+import {useRouter} from "vue-router";
+import {useUserState} from "@/stores/user";
 
+const userState = useUserState();
+const router = useRouter();
 const form = ref({userAccount: '', password: '', captchCode: '', reCode: '', captchaId: ''})
 onMounted(() => {
   getCodeURL();
@@ -33,9 +37,15 @@ const setCode = (data?: any) => {
   form.value.captchaId = data.captchaId;
   form.value.reCode = data.image
 }
+/**
+ * 登录
+ */
 const doLogin = () => {
   login(form.value).then((res?: any) => {
-    console.log(res)
+    if (res.data.code === 200) {
+      userState.setToken(res.data.data.token)
+      router.push('/admin');
+    }
   })
 }
 </script>
